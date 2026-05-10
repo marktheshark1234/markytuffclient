c8d43a8bb86a7c63683ad2204ef6d12cb7219d5b
marktheshark1234 <markymalarkyb@gmail.com>
2026-05-10 09:09:29 -0400

    Update input.js

--- api/input.js
+++ api/input.js
@@ -1,31 +1,83 @@
 export default async function handler(req, res) {
 
-	if(req.method !== "POST") {
-		return res.status(200).send("input ok");
-	}
+	try {
 
-	const { sessionId, key, time } = req.body;
-
-	const response = await fetch(
-		process.env.SUPABASE_URL + "/rest/v1/inputs",
-		{
-			method: "POST",
-			headers: {
-				apikey: process.env.SUPABASE_ANON_KEY,
-				Authorization:
-					`Bearer ${process.env.SUPABASE_ANON_KEY}`,
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				session_id: sessionId,
-				key: key,
-				time_ms: time
-			})
+		if(req.method !== "POST") {
+			return res
+				.status(200)
+				.send("input ok");
 		}
-	);
 
-	const text = await response.text();
+		console.log(
+			"BODY:",
+			req.body
+		);
 
-	res.status(200).send(text);
+		const {
+			sessionId,
+			key,
+			time
+		} = req.body;
+
+		const response =
+			await fetch(
+
+				process.env.SUPABASE_URL +
+
+				"/rest/v1/inputs",
+
+				{
+					method: "POST",
+
+					headers: {
+
+						apikey:
+							process.env.SUPABASE_ANON_KEY,
+
+						Authorization:
+							`Bearer ${process.env.SUPABASE_ANON_KEY}`,
+
+						"Content-Type":
+							"application/json"
+
+					},
+
+					body:
+						JSON.stringify({
+
+							session_id:
+								sessionId,
+
+							key:
+								key,
+
+							time_ms:
+								time
+
+						})
+
+				}
+
+			);
+
+		const text =
+			await response.text();
+
+		console.log(
+			"SUPABASE:",
+			text
+		);
+
+		res.status(200).send(text);
+
+	} catch(err) {
+
+		console.error(err);
+
+		res
+			.status(500)
+			.send(err.toString());
+
+	}
 
 }
